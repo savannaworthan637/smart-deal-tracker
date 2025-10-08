eb636cf1667420e725d0c5d211bd3ac8cae2a051
savannaworthan637 <129161002+savannaworthan637@users.noreply.github.com>
2025-10-08 10:40:20 +0500

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from routes import deals, users
+
+app = FastAPI(title="Smart Deal Tracker API")
+
+app.include_router(deals.router)
+app.include_router(users.router)
+
+@app.get("/")
+def root():
+    return {"status": "ok", "message": "Smart Deal Tracker API running"}
